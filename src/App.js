ed9a5e62f15a820343f163c2c89edf38d12c0cb8
rehan <rehantosif4@gmail.com>
2024-01-10 07:33:06 +0300

    new features

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>This is a new feature</p>
+        <p>Yayy! it worked</p>
         <a
           className="App-link"
           href="https://reactjs.org"
